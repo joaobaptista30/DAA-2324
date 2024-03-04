7a48837818809c5706745d96677b6a23efd27811
João Baptista <joao.baptista@outlook.pt>
2024-03-04 17:50:46 +0000

    .

--- Vol1/A.java
+++ Vol1/A.java
@@ -3,37 +3,20 @@ import java.util.LinkedList;
 
 public class A {
 
-
-
-
-
-
-
-
-
     public static void main (String[] strg){
 
     Scanner in = new Scanner(System.in);
 
-    LinkedList<Integer> a = new LinkedList<Integer>();
-
-    a.addFirst(10);
-    a.addLast(20);
-
-    int cur = a.getFirst();
-    int cur2 = a.get(1);
-    
-    System.out.println(a.size()+ " "+ cur+ " "+ cur2);
-    //input 
-
-    /*
+    //input
     int n = in.nextInt();
     int[] input_array = new int [n+1];
-    for (int i = 1; i < n+1; i++) {
-        input_array[i]= in.nextInt();
-    }
+    boolean[] checked = new boolean[n+1];
+    for (int i = 1; i < n+1; i++) input_array[i]= in.nextInt();
 
-    */
+    
+
+
+
 
     }
 }
